2fc04b7710af8adc9f1bb0f673541d47e2fe0f05
Dien Nguyen <nmdien61@gmail.com>
2015-09-03 16:22:37 +0700

    Resource Message in Spring Bean Configuration

--- spring-core/src/test/java/com/dee/web/spring/usecase4/MessagingBeanTest.java
+++ spring-core/src/test/java/com/dee/web/spring/usecase4/MessagingBeanTest.java
@@ -1,9 +1,33 @@
 package com.dee.web.spring.usecase4;
 
+import java.util.Calendar;
+import java.util.Locale;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 /**
  * @author dien.nguyen
  **/
 
-public class MessagingBeanTest {
+public class MessagingBeanTest extends TestCase {
+
+    private ApplicationContext appContext;
+
+    @Override
+    protected void setUp() throws Exception {
+        appContext = new GenericXmlApplicationContext("/spring/usecase4/message-resource-app-context.xml");
+    }
+
+    public void testMessageResource() {
+        Assert.assertEquals("USD", appContext.getMessage("currenncy.unit", null, Locale.US));
+        Assert.assertEquals("MM-dd-yyyy", appContext.getMessage("datetime.format", null, Locale.US));
+        
+        System.out.println(appContext.getMessage("datetime.message.today", 
+                new Object[] {Calendar.getInstance().getTime()}, Locale.US));
+    }
 
 }
